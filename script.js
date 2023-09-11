ba5abc4ba351e61b1eb974bc9ca4b669385d1ea9
Michael Rasolo <rasolon.michael@gmail.com>
2023-09-11 18:34:27 +0200

    game over + high score

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+// Space variables
 const gameSpace = document.querySelector(".game-container");
 let foodX = 10,
   foodY = 10;
+let gameSpeed;
 // Snake variables
 let headX = 3,
   headY = 1;
 let snakeBody = [];
-
 let directionX = 0,
   directionY = 0;
-const scoreInput = document.querySelector('.score')
-  let score = 0
+
+// Score variables
+const scoreInput = document.querySelector(".score");
+let score = 0;
+const highScoreInput = document.querySelector(".highscore");
+let highScore = localStorage.getItem("highscore") || 0;
+highScoreInput.textContent = highScore;
+
 function moveFood() {
   (foodX = Math.floor(Math.random() * 30) + 1),
     (foodY = Math.floor(Math.random() * 30) + 1);
@@ -36,44 +43,58 @@ function snakeDirection(e) {
   }
 }
 
+function stopGame() {
+  clearInterval(gameSpeed);
+  alert(`Game over, your score is ${score} !`);
+  location.reload();
+}
 
 function play() {
+  console.log("snakeBody", snakeBody);
   // Place food point
-
   let points = `<div class="food" style="grid-area: ${foodY}/${foodX}">  <div class="inner-food"></div></div>`;
 
-  // Update snake direction
+  // Update snake's head direction
   headX += directionX;
   headY += directionY;
 
-  // Snake ate food
+  // Eating event
   if (headX === foodX && headY === foodY) {
     moveFood();
-    snakeBody.push([foodY, foodX]);
-    // console.log("snakeBody", snakeBody);
-    console.log(foodY, foodX);
-    score++
-    scoreInput.textContent = score
+    snakeBody.push([]);
+    score++;
+    scoreInput.textContent = score;
+    if (score > highScore) {
+      highScore = score;
+      highScoreInput.textContent = highScore;
+      localStorage.setItem("highscore", highScore);
+    }
   }
 
-  // Body points take the position of the previous one
+  // Give each body point the position of the previous one
   for (let i = snakeBody.length - 1; i > 0; i--) {
     snakeBody[i] = snakeBody[i - 1];
   }
   snakeBody[0] = [headY, headX];
 
+  // Create a div for each body point
   for (let i = 0; i < snakeBody.length; i++) {
     points += `<div class="head" style="grid-area: ${snakeBody[i][0]}/${snakeBody[i][1]}"></div>`;
   }
   gameSpace.innerHTML = points;
 
-  
-  if (headX > 30 || headX < 1 ||headY > 30 || headY < 1) {
-    console.log("game over")
-    return
+  // Stop game if out of bounds
+  if (headX > 30 || headX < 1 || headY > 30 || headY < 1) {
+    stopGame();
+  }
+
+  // Stop game if body/head collision
+  for (let i = 1; i < snakeBody.length; i++) {
+    if (headY === snakeBody[i][0] && headX === snakeBody[i][1])
+      return stopGame();
   }
 }
 
 moveFood();
-setInterval(play, 100);
+gameSpeed = setInterval(play, 100);
 document.addEventListener("keydown", snakeDirection);
