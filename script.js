cd03a1278e9a08527407cc55d081c8f0ad8ba2a8
Michael Rasolo <rasolon.michael@gmail.com>
2023-09-11 15:56:00 +0200

    body+food, direction and score

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const gameSpace = document.querySelector(".game-container");
-let foodX = 0,
-  foodY = 0;
+let foodX = 10,
+  foodY = 10;
 // Snake variables
-let headX = 5,
-  headY = 10;
-let body = [];
+let headX = 3,
+  headY = 1;
+let snakeBody = [];
 
 let directionX = 0,
   directionY = 0;
-
+const scoreInput = document.querySelector('.score')
+  let score = 0
 function moveFood() {
   (foodX = Math.floor(Math.random() * 30) + 1),
     (foodY = Math.floor(Math.random() * 30) + 1);
@@ -20,38 +21,57 @@ function placeHead() {
 }
 
 function snakeDirection(e) {
-  console.log(e);
-  switch (e.key) {
-    case "ArrowDown":
-      (directionX = 1), (directionY = 0);
-      break;
-    case "ArrowUp":
-      (directionX = -1), (directionY = 0);
-      break;
-    case "ArrowLeft":
-      (directionX = 0), (directionY = -1);
-      break;
-    case "ArrowRight":
-      (directionX = 0), (directionY = 1);
-      break;
-    default:
-      break;
+  if (e.key === "ArrowDown" && directionY !== -1) {
+    directionX = 0;
+    directionY = 1;
+  } else if (e.key === "ArrowUp" && directionY !== 1) {
+    directionX = 0;
+    directionY = -1;
+  } else if (e.key === "ArrowLeft" && directionX !== 1) {
+    directionX = -1;
+    directionY = 0;
+  } else if (e.key === "ArrowRight" && directionX !== -1) {
+    directionX = 1;
+    directionY = 0;
   }
 }
 
+
 function play() {
-  let htmlPoints = `<div class="food" style="grid-area: ${foodX}/${foodY}">  <div class="inner-food"></div>
-    </div>`;
+  // Place food point
+
+  let points = `<div class="food" style="grid-area: ${foodY}/${foodX}">  <div class="inner-food"></div></div>`;
+
+  // Update snake direction
   headX += directionX;
   headY += directionY;
-//   
-  if (headX === foodX && headY === foodY){
-      moveFood();
-      body.push([foodX,foodY])
+
+  // Snake ate food
+  if (headX === foodX && headY === foodY) {
+    moveFood();
+    snakeBody.push([foodY, foodX]);
+    // console.log("snakeBody", snakeBody);
+    console.log(foodY, foodX);
+    score++
+    scoreInput.textContent = score
   }
 
-  htmlPoints += `<div class="head" style="grid-area: ${headX}/${headY}"></div>`;
-  gameSpace.innerHTML = htmlPoints;
+  // Body points take the position of the previous one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [headY, headX];
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    points += `<div class="head" style="grid-area: ${snakeBody[i][0]}/${snakeBody[i][1]}"></div>`;
+  }
+  gameSpace.innerHTML = points;
+
+  
+  if (headX > 30 || headX < 1 ||headY > 30 || headY < 1) {
+    console.log("game over")
+    return
+  }
 }
 
 moveFood();
